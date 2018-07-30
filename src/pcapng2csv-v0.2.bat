cd3179babe5d731fc52e7301c5f3230a55dc748a
Putu Ardi Dharmayasa <36402967+yasawibu@users.noreply.github.com>
2018-07-30 13:34:31 +0700

    Add files via upload

---
+++ src/pcapng2csv-v0.2.bat
@@ -0,0 +1,41 @@
+@ECHO OFF
+
+SET /P filter=<filter.txt
+SET column_header="No.","Time","Delta Time Displayed","Source","Destination","Protocol","Length","Info"
+
+IF EXIST "C:\Program Files (x86)\Wireshark\tshark.exe" (
+   SET tshark="C:\Program Files (x86)\Wireshark\tshark.exe"
+) ELSE IF EXIST "C:\Program Files\Wireshark\tshark.exe" (
+   SET tshark="C:\Program Files\Wireshark\tshark.exe"
+) ELSE (
+   ECHO Please install wireshark!
+)
+
+SET tshark_cmd=-Y "%filter%" -T fields -E separator=, -E quote=d -e frame.number -e frame.time_relative -e frame.time_delta_displayed -e _ws.col.Source -e _ws.col.Destination -e _ws.col.Protocol -e frame.len -e _ws.col.Info
+
+IF NOT EXIST csv\ (
+   MD csv
+)
+
+ECHO Converting pcapng to csv
+ECHO --------------------------------
+ECHO.
+
+FOR %%F IN (*.pcapng) DO (
+   ECHO Processing File: %%F
+   ECHO %column_header% > csv/%%~nF.csv
+   %tshark% -r %%F %tshark_cmd% >> csv/%%~nF.csv
+)
+
+ECHO.
+ECHO --------------------------------
+ECHO STATUS: Finished!
+ECHO.
+
+ECHO Thanks for using this program.
+ECHO.
+ECHO Created by Putu Ardi Dharmayasa.
+ECHO https://github.com/yasawibu
+ECHO.
+
+PAUSE
